c3478ab1d19b5ce4f5b6cfa00dfc2b09f3a159f4
risatkhandakar6-rs <risatkhandakar6@gmail.com>
2026-07-24 11:01:30 +0600

    active btn complete

---
+++ btn.js
@@ -0,0 +1,8 @@
+const btnElList = document.querySelectorAll('.btn');
+btnElList.forEach(btnEl => {
+  btnEl.addEventListener('click', () => {
+    btnElList.forEach(btn => btn.classList.remove('special'));
+    btnEl.classList.add('special')
+  });
+  
+});
