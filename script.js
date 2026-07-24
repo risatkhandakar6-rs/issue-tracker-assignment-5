9f4c09470ffb6acc06b314a491d0a938cc0fa381
risatkhandakar6-rs <risatkhandakar6@gmail.com>
2026-07-24 22:43:24 +0600

    final commit

--- script.js
+++ script.js
@@ -12,15 +12,15 @@ const loadAllIssue = () => {
     })
 }
 const getPriorityBadge = (priority) => {
-  if (priority === 'low') return 'bg-gray-100 text-gray-500'
-  if (priority === 'medium') return 'bg-yellow-100 text-yellow-500';
+  if (priority === 'low') return 'bg-pink-100 text-pink-500'
+  if (priority === 'medium') return 'bg-blue-100 text-blue-500';
   if (priority === 'high') return 'bg-red-100 text-red-500';
-  return 'bg-gray-100 text-gray-500'
+  return 'bg-pink-100 text-pink-500'
   
 }
 const labelColour = (label) => {
   if (label === 'bug') return 'bg-red-100 text-red-500';
-  if (label === 'help wanted') return 'bg-yellow-100 text-yellow-500';
+  if (label === 'help wanted') return 'bg-pink-100 text-pink-500';
   if (label === 'enhancement') return 'bg-green-100 text-green-500';
   if (label === 'good first issue') return 'bg-purple-100 text-purple-500';
   return 'bg-red-100 text-red-500';
@@ -32,7 +32,7 @@ const displayAllIssue = (issues) => {
   issues.forEach((data) => {
     const issueDiv = document.createElement('div');
     issueDiv.innerHTML = `
-     <div class="px-5 bg-[#FFFFFF] rounded-lg border-t-4 ${data.status === 'open'? 'border-green-300':'border-purple-300'}"> 
+     <div class="px-5 rounded-lg border-t-4 ${data.status === 'open'? 'border-green-300':'border-purple-300'}"> 
         <div class="flex justify-between mb-5 pt-5">
           <img src="${data.status === 'open' ? 'assets/Open-Status.png' : 'assets/Closed- Status .png'}" alt="" class="w-10 h-10 shrink-0">
           <h1 class="py-2 px-4 rounded-full ${getPriorityBadge(data.priority)}">${data.priority}</h1>
@@ -48,11 +48,50 @@ const displayAllIssue = (issues) => {
         </div>
       </div
     `;
+     issueDiv.addEventListener('click', () => {
+      displayModal(data);
+    });
+
+    
     issuesContainer.appendChild(issueDiv);
    
   })
+  const displayModal = (issue) => {
+    const modalContainer = document.getElementById('modal-container');
+    modalContainer.innerHTML = "";
+    const modalHtml = document.createElement('div');
+    modalHtml.innerHTML=`<h1 class="font-bold text-2xl pl-5 mb-3">${issue.title}</h1>
+        <div class="flex pl-5 ">
+          <h5 class=" rounded-full p-2 ${issue.status ==='open' ? 'bg-green-400' :'bg-purple-400'}">${issue.status}</h5>
+          <ul class="flex items-center gap-3 pl-5">
+            <li>. ${issue.author}</li>
+            <li>.${issue.createdAt}</li>
+          </ul>
+        </div>
+        <div class="flex gap-3 pt-10 pl-5">
+          ${issue.labels.map((label) => `<p class="${labelColour(label)} rounded-lg p-1">${label} </p>`).join("")}
+        </div>
+        <p class="pt-10 text-gray-500 pl-5">${issue.description }n</p>
+        <div class="flex justify-between w-11/12 bg-slate-100 mx-auto px-5 mt-7 p-6">
+          <div>
+            <p class="text-gray-600 font-bold">Assignee:</p>
+            <h4 class="font-bold ">${issue.assignee ? issue.assignee : "No Name"}</h4>
+          </div>
+          <div>
+            <p class="font-bold text-gray-600">Priority:</p>
+            <p class=" rounded-full p-4 ${getPriorityBadge(issue.priority)}">${issue.priority}</p>
+          </div>
+        </div>
+`
+    modalContainer.appendChild(modalHtml);
+    document.getElementById('my_modal_5').showModal();
+}
   
 }
+
+
+
+
 const manageSpinner = (load) => {
   if (load == true) {
     document.getElementById('loading').classList.remove('hidden');
